4bcbd78f818cde7d8ba44acaf319974e3833bb1b
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2021-09-20 12:48:19 +0200

    icons and images

--- src/pages/Icons.jsx
+++ src/pages/Icons.jsx
@@ -1,6 +1,6 @@
-import {ReactComponent as BME} from '../logos/bme.svg';
-import {ReactComponent as Android} from '../logos/android.svg';
-import {ReactComponent as Unity} from '../logos/unity.svg';
+import {ReactComponent as BME} from '../logos/pasta.svg';
+import {ReactComponent as Android} from '../logos/chicken.svg';
+import {ReactComponent as Unity} from '../logos/cookie.svg';
 import {ReactComponent as Bookr} from '../logos/bookr_logo.svg';
 import {ReactComponent as Group} from '../logos/group.svg';
 import {ReactComponent as Skill} from '../logos/skills.svg';

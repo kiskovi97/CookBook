2c0c5642d303bc8d96f201ca18ce7b95a442988b
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2021-09-20 12:32:27 +0200

    Main

--- src/pages/Components/Me.jsx
+++ src/pages/Components/Me.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import styles from './Me.module.css'
-import image from '../../images/profile_yellow.png'
+import image from '../../images/foodTemplate.png'
 import gStyles from '../Grid.module.css'
 import pStyles from '../Page.module.css'
 
 import insta from '../../logos/instagram.svg'
-import fb from '../../logos/facebook.svg'
-import linekdin from '../../logos/linkedin.svg'
 import github from '../../logos/github.svg'
 import ScrollAnimation from 'react-animate-on-scroll'
 
@@ -16,13 +14,11 @@ var Me = () =>
             <div className={styles.frontTitle}>
                 <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOutRight" animateOnce offset={20}>
                     <div className={styles.title} >
-                        Kovács Gergő
+                        Cooking Book
                     </div>
                 </ScrollAnimation>
                 <ScrollAnimation className={styles.logos} animateIn="fadeInLeft" animateOut="fadeOutLeft" offset={180} animateOnce>
                     <a href="https://www.instagram.com/kiskovi9706/" className={pStyles.minilogo}><img src={insta} alt=""/></a>
-                    <a href="https://www.facebook.com/gergo.kovacs.948" className={pStyles.minilogo}><img src={fb} alt=""/></a>
-                    <a href="https://www.linkedin.com/in/gerg%C5%91-kov%C3%A1cs-35215b154/" className={pStyles.minilogo}><img src={linekdin} alt=""/></a>
                     <a href="https://github.com/kiskovi97" className={pStyles.minilogo}><img src={github} alt=""/></a>
                 </ScrollAnimation>
             </div>
@@ -32,13 +28,14 @@ var Me = () =>
         </div>
         <div className={styles.description}>
             <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOutRight" animateOnce>
-                <h1>I'm a Software Engineer and Game Developer.</h1>
+                <h1>We are a nice couple, who loves to cook</h1>
             </ScrollAnimation>
             <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutLeft" offset={180} animateOnce> 
-                I make games for fun and for professionaly. 
-                I like to pursue intressting ideas and concepts and test them out first hand. 
-                If you are intressted in me or my work feel free to look around and contact me if you want.
-                I give great head ;)
+               We are Mark and Gergő. A simple couple from Hungary.
+               We love to cook (mostly on sundays) and post results on social media.
+               But we wanted to make a website too, where we can share the recepiets that we tried.
+               We post the original receipt and the one we modified. 
+               Please enjoy and feel free to contact us how your experients went.
             </ScrollAnimation>
         </div>
     </div>

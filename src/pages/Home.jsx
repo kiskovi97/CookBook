560059587ddab6af2911a7c53e1bac7ef621deda
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2021-09-28 13:45:25 +0200

    Selected buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,41 @@ import styles from './Page.module.css'
 import gStyles from './Grid.module.css'
 import Me from './Components/Me'
 import Receipt from './Components/Receipt'
-import { mainFive } from './filters';
+import dishes from './dishes.js';
 
-var Home = () => 
-<div className={styles.page}>
-    <Me/>
-    <div className={gStyles.grid_big}>
-        {mainFive.map((station, index) => (<Receipt proj={station} index={'/dessert/'+ index}/>))}
-    </div>
-</div>
+class Home extends React.Component {
+    state = {
+        filter: (item) => true,
+        top: false,
+        all: true,
+    }
+
+    onAllClicked = () => {
+        var filterFunc = (item) => true;
+        this.setState({filter: filterFunc, all: true, top: false});
+    }
+
+    onTopClicked = () => {
+        var filterFunc = (item) => true;
+        this.setState({filter: filterFunc, all: false, top: true});
+    }
+
+    render () {
+        let receipts = dishes.filter(this.state.filter);
+        if (this.state.top) 
+            receipts = receipts.slice(0,5);
+        return(
+            <div className={styles.page}>
+                <Me/>
+                <div className={styles.filters}>
+                    <button type="button" onClick={this.onAllClicked} className={this.state.all ? styles.selected:""}>All</button>
+                    <button type="button" onClick={this.onTopClicked} className={this.state.top ? styles.selected:""}>Top 5</button>
+                </div>
+                <div className={gStyles.grid_big}>
+                    {receipts.map((station, index) => (<Receipt proj={station} index={'/dessert/'+ index}/>))}
+                </div>
+            </div>)
+    }
+}
+
 export default Home

a147d722557cac6a685fb84271f30469c3f36665
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2021-11-27 14:15:54 +0100

    Beginning page

--- src/pages/Components/Me.jsx
+++ src/pages/Components/Me.jsx
@@ -13,8 +13,11 @@ var Me = () =>
         <div className={styles.titleBlock}>
             <div className={styles.frontTitle}>
                 <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOutRight" animateOnce offset={20}>
-                    <div className={styles.title} >
-                        Cooking Book
+                    <div className={styles.title} hidden={!navigator.language.startsWith("en")}>
+                        Legény fogó
+                    </div>
+                    <div className={styles.title} hidden={!navigator.language.startsWith("hu")}>
+                        Husband Material
                     </div>
                 </ScrollAnimation>
                 <ScrollAnimation className={styles.logos} animateIn="fadeInLeft" animateOut="fadeOutLeft" offset={180} animateOnce>
@@ -28,14 +31,23 @@ var Me = () =>
         </div>
         <div className={styles.description}>
             <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOutRight" animateOnce>
-                <h1>We are a nice couple, who loves to cook</h1>
+                <h1 hidden={!navigator.language.startsWith("hu")}>"Most már férjhez mehetsz"</h1>
+                <h1 hidden={!navigator.language.startsWith("en")}>"You can get married now"</h1>
             </ScrollAnimation>
             <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutLeft" offset={180} animateOnce>
-                We are Mark and Gergő. A simple couple from Hungary.
-                We love to cook (mostly on sundays) and post results on social media.
-                But we wanted to make a website too, where we can share the recepiets that we tried.
-                We post the original receipt and the one we modified.
-                Please enjoy and feel free to contact us how your experients went.
+                <div hidden={!navigator.language.startsWith("hu")}>
+                    Van egy olyan mondás miszerint, ha egy étel igazán jól sikerül, akkor azt szokás mondani "most már férjhez mehetsz".
+                    Mivel mi is megkaptuk ezt már egy párszor, úgy gondoltuk megosztjuk veletek azokat a recepteket,
+                    amelyekkel ti is hasonló sikert érhettek el.
+                </div>
+                <div hidden={!navigator.language.startsWith("en")}>
+                    When you cook a really good meal in Hungary, than you often get the compliment: "You can get married now".
+                    We got this a couple of times, so we thaught we should share the recipes that earned this compliment.
+                    Please enjoy and feel free to contact us how your experiences went.
+                </div>
+                <div className={styles.names}>
+                    Gergő & Mark
+                </div>
             </ScrollAnimation>
         </div>
     </div>

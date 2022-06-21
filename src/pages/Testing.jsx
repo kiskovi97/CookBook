23b3f6450b3a237d103611760cdb7b3e7637a870
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2022-06-21 14:34:11 +0200

    new test

--- src/pages/Testing.jsx
+++ src/pages/Testing.jsx
@@ -15,7 +15,7 @@ var Testing = () =>
                 <h3><a href="bookrclass://?book=2&ssoid=vcloud&token=teacher-viewrestricted-exit"target="_blank" rel="noreferrer">BookrDeeplink - book = 2 and teacher-viewrestricted-exit</a></h3>
                 <h3><a href="bookrclass://?book=2"target="_blank" rel="noreferrer">BookrDeeplink - book = 2</a></h3>
                 <h3><a href="bookrclass://?cloubotToken=iscream_cloubot_8bf23a4efe30d6e0e7095821999f302e1c212f65e6db23cdeba663114157d222&oauthUrl=https://home-learn-elibrary-lms.cloubot.com/member/oauthElibraryFromToken"target="_blank" rel="noreferrer">Cloubot test</a></h3>
-                
+                <h3><a href="bookrclass://?cloubotToken=iscream_museham90_c09bba7e3da925dd5f6480c2e623f9c9cc1d84d8edb88d9ad558bc541685a92b&oauthUrl=https://home-learn-elibrary-lms.cloubot.com/member/oauthElibraryFromToken"target="_blank" rel="noreferrer">Cloubot test</a></h3>
         </div>
     </div>
 

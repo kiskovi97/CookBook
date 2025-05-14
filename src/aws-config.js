d9fe6ff2b1e819dc3dadd0e5c908cc717b0ccb38
Gergo Kovacs <gergo.kovacs@breakpoint.one>
2025-05-14 10:40:58 +0200

    Fix 2

--- src/aws-config.js
+++ src/aws-config.js
@@ -3,7 +3,7 @@ import AWS from "aws-sdk";
 AWS.config.update({
   region: 'us-east-1',
   credentials: new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'us-east-1:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+    IdentityPoolId: 'us-east-1:87ee00f8-14fd-4750-9093-d849eacaaee4',
   }),
 });
 

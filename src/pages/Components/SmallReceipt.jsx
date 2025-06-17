14401c0b25ee470d73c8a277fc308a6ff234527d
Gergo Kovacs <gergo.kovacs@breakpoint.one>
2025-06-17 10:02:18 +0200

    fix

--- src/pages/Components/SmallReceipt.jsx
+++ src/pages/Components/SmallReceipt.jsx
@@ -1,17 +1,15 @@
 import styles from './SmallReceipt.module.css'
 import ScrollAnimation from 'react-animate-on-scroll'
-import { useNavigate  } from 'react-router';
+import { Link } from 'react-router'
 
 const SmallReceipt = ({proj, hidden}) => {
-    const navigate = useNavigate();
-    const handleClick = (index) => navigate("/" + index);
 
     var imageLink = proj?.image?.replace("static/media", "images");
     var id = proj.id;
     return (
         <div hidden={hidden}>
             <ScrollAnimation animateIn={"fadeIn"} animateOnce duration={0.6} offset={0}>
-                <div className={styles.receipt} onClick={() => handleClick("dbdish/" + id)} >
+                <Link className={styles.receipt} to={"dbdish/" + id} >
                     <div className={styles.image} >
                         <img src={imageLink} hidden={!proj.image} alt="" className={styles.background} />
                     </div>
@@ -19,7 +17,7 @@ const SmallReceipt = ({proj, hidden}) => {
                         <div className={styles.title}>{proj.title}</div>
                         <div className={styles.details}>{proj.details}</div>
                     </div>
-                </div>
+                </Link>
             </ScrollAnimation>
         </div>
         )

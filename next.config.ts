24c892a3b9d94587c947c5bd4b6669773b17363c
Gergo Kovacs <kiskovi97@gmail.com>
2025-09-07 10:20:23 +0200

    enable all images

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,11 @@ const nextConfig: NextConfig = {
   //output: 'export', // Outputs a Single-Page Application (SPA)
   //distDir: 'build', // Changes the build output directory to `build`
   images: {
-    domains: [
-      'kiskovi97.github.io', 
-      'streetkitchen.hu', 
-      'img-global.cpcdn.com', 
-      "img-global.cpcdn.com", 
-      "azureskproductionfs.blob.core.windows.net",
-      "static.vecteezy.com",
-      "p16-pu-sign-no.tiktokcdn-eu.com",
-      "p16-common-sign-no.tiktokcdn-us.com",
-      "lh3.googleusercontent.com",
-      "firebasestorage.googleapis.com"
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 }
